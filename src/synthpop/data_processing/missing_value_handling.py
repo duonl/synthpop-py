0b8d485f00b9f51b8158ac7dd0f9d1ca440ee674
Erina <erina.koomen@duo.nl>
2026-02-10 15:45:04 +0100

    applied suggestions in #15

---
+++ src/synthpop/data_processing/missing_value_handling.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod,ABCMeta
+import pandas as pd
+
+class BaseMissingValueHandler(metaclass=ABCMeta):
+    """
+    Base class for different strategies to handle missing values in the target variable.
+    """
+
+    @abstractmethod
+    def prepare_data_for_fit(self,X:pd.DataFrame,y:pd.Series) -> tuple[pd.DataFrame,pd.Series]:
+        """
+        Prepare the feature and/or target for fitting.
+        :param X: the features for the target. Implementors should accept both categoric and nummeric data, and should accept missing values here.
+        :param y: the target. May contain missing values. Implementors do not need to accept both categorical and nummeric targets, but should accept one of them.
+
+        :return: a tuple (X,y) of data ready to be further processed and used for fitting a model. the second item of the tuple (y) may not contain missing values.
+        X may contain missing values.
+        """
+        pass
+
+    @abstractmethod
+    def post_synth_transform(self,x:pd.DataFrame,y:pd.Series) -> pd.Series:
+        """
+        Process synthesised data to include missing values.
+        :param X: the features for the target. Implementors should accept both categoric and nummeric data, and should accept missing values here.
+        :param y: the target, should not contain missing values. 
+
+        :return:  The synthesised target with missing values.
+        """ 
+        pass
+    
+
+class MissingValuePredictor(BaseMissingValueHandler):
+    
+    def prepare_data_for_fit(self,X:pd.Series, y:pd.Series)-> tuple[pd.DataFrame,pd.Series]:
+        """
+        Trains a decision tree to predict when y is missing. Removes rows from both X and y when y is missing.
+        :param X: the features for the target. 
+        :param y: the target.
+
+        :return: a tuple (X,y) of data ready to be further processed and used for fitting a model. 
+        """
+        
+        return(pd.DataFrame(),pd.Series())
+
+    def post_synth_transform(self,X:pd.DataFrame,y:pd.Series) -> pd.Series:
+        """
+        Uses a decision tree to determine when y should be missing.
+        :param X: the features for the target.
+        :param y: the target
+
+        :return:  The synthesised target with missing values.
+        """ 
+        return pd.Series()
