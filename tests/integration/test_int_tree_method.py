16cf2ef9d155f16a717dc449a1a0b6301b20541e
Erina <erina.koomen@duo.nl>
2026-04-09 13:33:37 +0200

    added integration test for regressor method

--- tests/integration/test_int_tree_method.py
+++ tests/integration/test_int_tree_method.py
@@ -2,7 +2,7 @@ import pytest
 import pandas as pd
 import numpy as np
 
-from synthpop.methods.cart_synth import TreeClassifierMethod
+from synthpop.methods.cart_synth import TreeClassifierMethod, TreeRegressorMethod
 
 def test_treemethod_classifier_fit_and_transform():
     tree_method = TreeClassifierMethod()
@@ -13,6 +13,24 @@ def test_treemethod_classifier_fit_and_transform():
         "column3":np.array(["a","b"])
         }
     y = np.array(["x","y"])
+
+    tree_method.fit(X,y)
+    assert tree_method.n_features_in_ >= 3
+
+    result = tree_method.transform(X)
+
+    assert result.shape[0] ==2
+
+
+def test_treemethod_regressor_fit_and_transform():
+    tree_method = TreeRegressorMethod()
+
+    X = {
+        "column1":np.array([1.1,2.2]),
+        "column2":np.array([1.4,1.2]),
+        "column3":np.array(["a","b"])
+        }
+    y = np.array([1,2])
 
     tree_method.fit(X,y)
     assert tree_method.n_features_in_ >= 3
