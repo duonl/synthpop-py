0bbcec5953bcad413dd85bc0e7091007940712d9
Erina <erina.koomen@duo.nl>
2026-04-20 16:05:14 +0200

    implemented changing seeds

--- src/synthpop/reproducibility.py
+++ src/synthpop/reproducibility.py
@@ -3,29 +3,10 @@ import secrets
 import numpy as np
 from numpy.random import SeedSequence
 
-# Depending on the user for the root seed is suboptimal.
-# Humans tend to not be so random. And forgetting to set a seed is too easy. 
-# The seed 165204380881971836312753519450099508068 is a random number I would not have thought of.
-
-# The proposed solution is to check if a seed has already been set (by existence of 'seed.txt').
-# If the file exists, read its content and use it as a seed.
-# If that file does not exists, generate a new seed of 128 bits and write it to 'seed.txt'
-# This seeding happens when this file is imported, so the user does not need to remember to set a seed.
-
-
-
-if os.path.isfile("seed.txt"):
-    with open("seed.txt","r") as f:
-        _root_seed = int(f.read())
-else:
-    _root_seed = secrets.randbits(128)
-    with open("seed.txt","w") as f:
-        f.write(str(_root_seed))
-
+_root_seed = None
 _root_rng = np.random.default_rng(_root_seed)
 _seed_sequence = SeedSequence(_root_seed)
 
-print(_root_rng.integers(0,100,10))
 
 
 # now that we have a reproducible RNG, the rest of the package needs to use it.
@@ -46,5 +27,44 @@ def get_seed():
 # Another possibility is to provide RNGS to the other classes directly. This keeps the responsibility of creating RNGs in this module.
 # It means we can alter the strategy for random numbers without altering the classes using them. 
 # A disadvantage is that all classes are forced to accept the type of RNG that this module provides.
-def get_rng():
-    return _root_rng.spawn(1)[0]
+
+# The rng returned by this function should depend on the _root_seed and the seed parameter.
+# same _root_seed + same seed => same RNG, for reproducibility. 
+def get_rng(seed):
+    return np.random.default_rng(_root_seed + seed)#_root_rng.spawn(1)[0]
+
+
+class Reseed():
+
+    _root_seed = None
+    _seed_sequence = None
+
+    @classmethod
+    def set_root_seed(cls,seed,seed_sequence = None):
+        cls._root_seed = seed
+        if seed_sequence is None:
+            cls._seed_sequence = SeedSequence(seed)
+        else:
+            cls._seed_sequence = seed_sequence
+
+    @classmethod
+    def get_seed(cls):
+        return cls._seed_sequence.spawn(1)[0]
+
+
+    @classmethod
+    def get_rng(cls,seed):
+        return np.random.default_rng(SeedSequence(entropy=(cls._root_seed + seed.entropy)))
+
+
+    def __init__(self,seed):
+        self.new_seed = seed
+        self.old_seed = None
+
+    def __enter__(self):
+        self.old_seed = Reseed._root_seed
+        self.old_seed_sequence = Reseed._seed_sequence
+        Reseed.set_root_seed(self.new_seed)
+
+    def __exit__(self, type, value, traceback):
+        Reseed.set_root_seed(self.new_seed,self.old_seed_sequence)
