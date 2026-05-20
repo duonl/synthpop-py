efc9dc1a7a7826ffa515bd608d27101930d21ddf
Erina <erina.koomen@outlook.com>
2026-05-20 13:16:20 +0200

    merge conflict resolve

--- src/synthpop/data_processing/encoders.py
+++ src/synthpop/data_processing/encoders.py
@@ -288,7 +288,7 @@ class MeanEncoder(_BaseEncoder):
             >>> encoder.fit(X, y)
         """
 
-        if not (np.issubdtype(y.dtype,np.number) or y.dtype == np.bool_):
+        if not pd.api.types.is_numeric_dtype(y):
             raise ValueError(f"MeanEncoder requires numeric target array y. Received dtype={y.dtype}")
 
         if X.shape[0] == 0 or y.shape[0] == 0:
