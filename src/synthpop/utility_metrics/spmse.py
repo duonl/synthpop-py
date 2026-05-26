51cbcbdeaab299ed6fee4833b3017d3e17a4484c
casvanerpduo <cas.vanerp@duo.nl>
2026-05-26 16:16:46 +0200

    First version of S_pMSE

--- src/synthpop/utility_metrics/spmse.py
+++ src/synthpop/utility_metrics/spmse.py
@@ -2,8 +2,42 @@
 This module contains metrics to evaluate the utility of synthetic data.
 """
 import pandas as pd
+from itertools import combinations_with_replacement
+import numpy as np
 
-def pairwise_spmse(orig_df: pd.DataFrame, syn_df: pd.DataFrame, max_groups: int = 25, na_label: str = "__NA__") -> pd.DataFrame:
+def preprocessing(df, max_bins=25):
+    for colname in df:
+        if pd.api.types.is_numeric_dtype(df[colname]):
+            binned_column = pd.cut(df[colname],max_bins, labels=range(max_bins)) 
+            df[colname] =binned_column
+    return df
+
+def joint_frequencies(df, name):
+    """
+    Create pairwise joint frequency tables for all variable pairs.
+    """
+    results = {}
+    for col1, col2 in combinations_with_replacement(df.columns, 2):
+        if col1 == col2:
+            group = (df.groupby(col1, dropna=False).size().reset_index(name=name))
+        else:
+            group = (df.groupby([col1, col2], dropna=False).size().reset_index(name=name))
+
+        results[(col1, col2)] = group
+    return results
+
+def Calc_S_pSME(jf_or: pd.DataFrame, jf_syn: pd.DataFrame, n_o: int, n_s: int, f_or: str,  f_syn: str):
+
+    rescaled_differences = jf_syn[f_syn]-n_s/n_o*jf_or[f_or]
+    expected_frequency = (jf_or[f_or]+jf_syn[f_syn])*(n_s/(n_o+n_s))
+    k = (expected_frequency>0)
+    if k.sum()>1:
+        S_pMSE = 1/(k.sum()-1)*np.nansum(rescaled_differences[k]**2)/np.nansum(expected_frequency[k])
+    else:
+        raise ValueError("There is only one unique category pair in the data")
+    return S_pMSE
+
+def pairwise_spmse(orig_df: pd.DataFrame, syn_df: pd.DataFrame, max_bins: int = 25, na_label: str = "__NA__") -> pd.DataFrame:
     """
     Compute the Standardized propensity Mean Squared Error (S_pMSE) as defined in [1] for all pairs of variables 
     between two similarly-structured dataframes: one original dataset and one synthetic version of the original dataset,
@@ -13,14 +47,43 @@ def pairwise_spmse(orig_df: pd.DataFrame, syn_df: pd.DataFrame, max_groups: int 
     :type orig_df: pd.DataFrame
     :param syn_df: Synthetic dataset. Should have the same columns as the original dataset, in the same order and the same number of rows.
     :type syn_df: pd.DataFrame
-    :param max_groups: Maximum number categories in which numeric variables can be discretized. Default value is 25.
-    :type max_groups: int
+    :param max_bins: Maximum number categories in which numeric variables can be discretized. Default value is 25.
+    :type max_bins: int
     :param na_label: String value to be given to missing values. Default is __NA__
     :type na_label: str
     :return: Dataset of variable pairs along with their corresponding S_pMSE value.
     :rtype: DataFrame
     """
-    if orig_df.shape != syn_df.shape:
-        raise ValueError("Original and synthetic dataframes must have the same shape.")
-    output = orig_df.copy()
-    return output
+    
+    if not isinstance(orig_df, pd.DataFrame) or not isinstance(syn_df, pd.DataFrame):
+        raise ValueError("Original and synthetic dataframes should both be a pandas dataframe")
+    
+    n_o = len(orig_df.index)
+    n_s = len(syn_df.index)
+
+    if n_o == 0 or n_s ==0:
+        raise ValueError('Both the original and synthetic dataframe should consist out of non-zero rows')
+    if len(orig_df.columns) != len(syn_df.columns) or not all(orig_df.columns==syn_df.columns):
+        raise ValueError("Original and synthetic dataframes must have the same shape and column names.")
+    if max_bins <= 1 or not isinstance(max_bins, int):
+        raise ValueError("The number of bins should be a positive integer.")
+
+    """Functional parameters"""
+    f_origxy= 'f_or'
+    f_synthxy= 'f_syn'
+    #Wellicht kan dit ook als onderdeel van een class. Maar idk of het 'omgooien' nodig is. In dat geval kan je ook de tabellen e.d. opslaan als self. ...
+
+    """Start calculations here"""
+    binned_orig, binned_synth = preprocessing(orig_df.copy(),max_bins=max_bins), preprocessing(syn_df.copy(),max_bins=max_bins)
+    #Hier ontbreekt nog een NaN handling
+
+    joint_frequencies_orig = joint_frequencies(binned_orig, name=f_origxy)
+    joint_frequencies_synth = joint_frequencies(binned_synth, name=f_synthxy)
+    S_pMSEdict = {}
+    for key in joint_frequencies_orig:
+        S_pMSEdict[key] = Calc_S_pSME(joint_frequencies_orig[key], joint_frequencies_synth[key], n_o, n_s,  f_origxy, f_synthxy)
+
+    S_pMSEdf = pd.DataFrame(S_pMSEdict, index=[0]) #make it a dataframe
+    correct_form = S_pMSEdf.T.rename_axis(["column1", "column2"]).reset_index().rename(columns={0: 'S_pMSE'}) #make it a dataframe with 3 colums: col1, col2, S_pMSE
+
+    return correct_form
--- tests/unit/test_S_pMSE.py
+++ tests/unit/test_S_pMSE.py
@@ -2,23 +2,98 @@ import pandas as pd
 import numpy as np
 import pytest
 
-from synthpop.utility_metrics.spmse import pairwise_spmse
+from synthpop.utility_metrics.spmse import pairwise_spmse, preprocessing, joint_frequencies, Calc_S_pSME
 
 @pytest.mark.parametrize(
     "orig_df, syn_df, expected",
     [
-        (pd.DataFrame(), pd.DataFrame(), pd.DataFrame()), #Check for empty dataframes
-        (pd.DataFrame([[1,2],[3,4]]), pd.DataFrame([[1,2],[3,4]]), pd.DataFrame([[0,0],[0,0]])) #Check for identical dataframes
-    ] 
+        (pd.DataFrame([[1,2],[3,4]], columns=['c1', 'c2']), 
+        pd.DataFrame([[1,2],[3,4]], columns=['c1', 'c2']), 
+        pd.DataFrame([['c1', 'c1', 0],['c1', 'c2', 0],['c2','c2',0]], columns=['column1', 'column2', 'S_pSME'])) #Desired output format.
+    ] #The S_pSME should be 0 for all three as the synthetic and original are the exact same!
 )
-def test_pairwise_spmse_input(orig_df, syn_df, expected):
+def test_pairwise_spmse_output(orig_df, syn_df, expected):
 
     output = pairwise_spmse(orig_df, syn_df)
+
     assert output.equals(expected)
 
-def test_pairwise_spmse_shapemismatch():
-    orig_df = pd.DataFrame([[1,2],[3,4]])
-    syn_df = pd.DataFrame([[1,2,3],[4,5,6]])
+@pytest.mark.parametrize(
+    "orig_df, syn_df, max_bins",
+    [
+        (pd.DataFrame([[1,2],[3,4]]), pd.DataFrame([[1,2,3],[4,5,6]]), 25), #Check for unequal number of columns
+        (pd.DataFrame({"A": [10], "B": [20]}), pd.DataFrame({"A": [10], "C": [20]}), 25), #Check column names not equal
+        ([],[], 12), #Check for non pandas dataframes
+        (pd.DataFrame(), pd.DataFrame(), 25.), #Check if max_bins is not an integer
+        (pd.DataFrame(), pd.DataFrame(), -12), #Check for negative bins
+        (pd.DataFrame(), pd.DataFrame(), 35) #Check empty DataFrames
+    ] 
+)
+def test_pairwise_spmse_inputtests(orig_df, syn_df, max_bins):
     with pytest.raises(ValueError):
-        pairwise_spmse(orig_df, syn_df)
+        pairwise_spmse(orig_df, syn_df, max_bins)
+
+def test_preprocessing():
+    df = pd.DataFrame({
+        "a": [0, 25, 50, 75, 100],
+        "b": ["this", "is", "a", "test", "case"]
+    })
+    num_bins=10
     
+    result = preprocessing(df.copy(), max_bins=num_bins)
+    expected_a = pd.cut(df["a"], num_bins, labels=range(num_bins)) #run the pd cut on numeric column
+    
+    assert result["a"].tolist() == expected_a.tolist()
+    assert result["b"].tolist() == df["b"].tolist() #check if string column remains unaffected
+
+@pytest.mark.parametrize(
+    "df_input, output",
+    [
+    (pd.DataFrame({"c1" : [0,0]}), {('c1', 'c1'): pd.DataFrame({"c1" : [0], 'x': [2]})}),
+    (pd.DataFrame({"c1" : [0,0], "c2" : [1,1]}), {('c1', 'c1'): pd.DataFrame({"c1" : [0], 'x': [2]}),
+                                                    ('c1', 'c2'): pd.DataFrame({"c1" : [0], "c2" : [1], 'x': [2]}),
+                                                    ('c2', 'c2'): pd.DataFrame({"c2" : [1], 'x': [2]})})
+    ] 
+)
+def test_joint_frequencies(df_input,output):
+    res = joint_frequencies(df_input, 'x')
+    for k in output:
+        assert k in res
+        assert res[k].equals(output[k])
+
+@pytest.mark.parametrize(
+    "orig_df, syn_df, should_raise",
+    [
+        ({('c1', 'c1'): pd.DataFrame({"c1" : [0], 'f_or': [2]})}, {('c1', 'c1'): pd.DataFrame({"c1" : [0], 'f_syn': [2]})}, True),     #THIS SHOULD RAISE AN ERROR as k=1
+        
+        ({('c1', 'c2'): pd.DataFrame({"c1" : [0,1],  "c2" : [2,3],'f_or': [0,0]})},
+        {('c1', 'c2'): pd.DataFrame({"c1" : [0,1], "c2" : [2,3], 'f_syn': [0,0]})}, 
+        True), #THIS SHOULD RAISE AN ERROR as E will give negative numbers
+
+        ({('c1', 'c1'): pd.DataFrame({"c1" : [0,1], 'f_or': [2,2]}), 
+        ('c1', 'c2'): pd.DataFrame({"c1" : [0,1], "c2" : [1,2], 'f_or': [2,2]}),
+        ('c2', 'c2'): pd.DataFrame({"c2" : [1,2], 'f_or': [2,2]})},
+        
+        {('c1', 'c1'): pd.DataFrame({"c1" : [0,1], 'f_syn': [2,2]}), 
+        ('c1', 'c2'): pd.DataFrame({"c1" : [0,1], "c2" : [1,2], 'f_syn': [2,2]}),
+        ('c2', 'c2'): pd.DataFrame({"c2" : [1,2], 'f_syn': [2,2]})},
+        False) #In this case f_orig=f_synth, so outcome statistic should be 0!
+
+    ] 
+)
+def test_calc_S_pMSE(orig_df, syn_df, should_raise):
+    n_s, n_o = 2, 2
+    f_origxy= 'f_or'
+    f_synthxy= 'f_syn'
+    if should_raise:
+        with pytest.raises(ValueError):
+            for key in orig_df:
+                Calc_S_pSME(orig_df[key], syn_df[key], n_o, n_s,  f_origxy, f_synthxy)
+    else:
+        res = {}
+        for key in orig_df:
+            res[key] = Calc_S_pSME(orig_df[key], syn_df[key], n_o, n_s,  f_origxy, f_synthxy)
+        assert all(v == 0. for v in res.values())
+    return
+
+
