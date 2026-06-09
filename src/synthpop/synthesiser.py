008a64a1ec09181eca0c8c3e478161469456a43a
Erina <erina.koomen@duo.nl>
2026-06-09 11:46:23 +0200

    improved docstrings

--- src/synthpop/synthesiser.py
+++ src/synthpop/synthesiser.py
@@ -16,7 +16,7 @@ class Synthesiser:
 
     :param random_seed: seed for randomness.
     :param column_order: list of variable names or list of indexes to define the order in which the columns will be synthesised. Default is the column order of the original dataset.
-    :param default_syn_method: BaseSynth object. Synthesis method to apply to each column, except the first one and the ones defined in special_syn_method. Default synthesis method is CartSynth. 
+    :param default_syn_method: Synthesis method to apply to each column, the ones defined in special_syn_method. Default synthesis method is CartSynth. 
     :param special_syn_method: Dictionary of special synthesis method per variable. If some variables should not follow the default_syn_method, they should be indicated in a dictionary where keys are variable names and values are BaseSynth objects. By default, there is no special synthesis method.
 
     Examples
@@ -101,8 +101,8 @@ class Synthesiser:
         """
         Loops through the columns in ``X``, following ``column_order``, and calls the :py:meth:`fit` function of the synthesis method classes given in ``default_syn_method`` and ``special_syn_method``.
         
-        For the first column, nothing happens. For the next ones, if the variable name is found in keys of ``special_syn_method``, its corresponding value is the class to be called to fit the synthesiser.
-        Otherwise, we use the class defined in ``default_syn_method``.
+        If the variable name is found in keys of ``special_syn_method``, its corresponding value is the object used to synthesise that variable. The `fit` method of that object will be called.
+        Otherwise, we use the object defined in ``default_syn_method``.
 
         :param X: An original dataset on which to fit the synthesiser.
         :param y: Ignored. This parameter exists only for compatibility with sklearn estimators.
@@ -154,11 +154,8 @@ class Synthesiser:
         """
         Generate a synthetic dataset of ``n`` rows. 
 
-        This method loops through the columns of ``X``, following ``column_order``, and calls the :py:meth:`transform` function of the synthesis method classes from ``default_syn_method`` and ``special_syn_method``.
+        This method loops through the columns of ``X``, following ``column_order``, and calls the :py:meth:`transform` function of the synthesis method objects as used in `fit`.
         
-        For the first column, the method specified in ``first_column_method`` is used. For the next ones, we use their respective fitted functions. Each column is predicted
-        using the previously generated columns as features.
-
         :param n: Number of rows to generate for the synthetic dataset. Default is the same number of rows than the dataset on which the synthesiser was fitted. If one of the synthesis methods copies the original data, this parameter must be None.
         :param random_state: Random seed generator. Default is 42. 
         
