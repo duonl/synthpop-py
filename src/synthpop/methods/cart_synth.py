7535234189c2b12c06f5fcb97d342196d87a70d1
KoomenErina <erina.koomen@duo.nl>
2026-06-12 14:00:26 +0200

    Apply suggestion from @anneliesdej
    Co-authored-by: anneliesdej <113341080+anneliesdej@users.noreply.github.com>

--- src/synthpop/methods/cart_synth.py
+++ src/synthpop/methods/cart_synth.py
@@ -460,7 +460,9 @@ def tune_cart(n_leaves: int = 5, n_components: int | float | None = None) -> Car
     >>> from synthpop.synthesiser import Synthesiser
     >>> import pandas as pd
     >>> data = pd.DataFrame({"a":[1],"b":[2]})
-    >>> synth = Synthesiser(random_seed=10,default_syn_method = tune_cart(n_leaves=10), special_syn_method= { "b": tune_cart(n_leaves=20)})
+    >>> synth = Synthesiser(random_seed=10,
+    ... default_syn_method=tune_cart(n_leaves=10), 
+    ... special_syn_method={"b": tune_cart(n_leaves=20)})
 
     """
     return CartMethod(
