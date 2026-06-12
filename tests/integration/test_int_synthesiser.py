2c53c14944c5c48b381826fa6c3f6fb68146c0c6
KoomenErina <erina.koomen@duo.nl>
2026-06-12 13:38:37 +0200

    130 quickfix rng (#139)
    * started with testing
    
    * started on making a stub for the synth method
    
    * worked on test
    
    * default synthesis implemented
    
    * default generate works
    
    * cleaned tests
    
    * custom synthesis order
    
    * synthesis order when generating
    
    * made integration tests
    
    * test
    
    * made test
    
    * implemented error on not a dataframe
    
    * Apply suggestions from code review
    
    Co-authored-by: anneliesdej <113341080+anneliesdej@users.noreply.github.com>
    
    * applied small feedback
    
    * test commit and push
    
    * made unit test
    
    * made test passing
    
    * added integration test for sampling the first column
    
    * made input validation for column order
    
    * renamed argument of generate
    
    * validated 1D distributions
    
    * tested 2D relations
    
    * added example
    
    * improved docstrings
    
    * Apply suggestions from code review
    
    Co-authored-by: anneliesdej <113341080+anneliesdej@users.noreply.github.com>
    
    * autopep8
    
    * autopep8 for integration tests
    
    * tested for negative indices
    
    * applied quickfix
    
    * adjusted tests
    
    * improved probabilities of tests
    
    * enabled test that demonstrated the bug
    
    * altered test to use more rows
    
    ---------
    
    Co-authored-by: anneliesdej <113341080+anneliesdej@users.noreply.github.com>

--- tests/integration/test_int_synthesiser.py
+++ tests/integration/test_int_synthesiser.py
@@ -53,7 +53,7 @@ def test_synthesiser_first_column_is_sampled_categorical():
         expected_proportions["missing"] - result_proportions[np.nan]) < 0.05
 
 
-@pytest.mark.xfail(reason="known issue, see #130")
+#@pytest.mark.xfail(reason="known issue, see #130")
 def test_synthesiser_first_column_is_sampled_numeric():
     expected_proportions = {
         '1.1': 1/2,
@@ -131,8 +131,8 @@ def test_synthesiser_preserves_1D_statistics():
     The goal is to test that the synthetic data is reasonable. Benchmarking for utility should happen in other tests.
     """
 
-    n_samples_orig = 1000
-    n_samples_synthetic = 2000
+    n_samples_orig = 5000
+    n_samples_synthetic = 6000
     original_data, index_num, index_cat = simulate_realistic_dataset_correlations(
         n_samples=n_samples_orig)
     synthesiser = Synthesiser(random_seed=74124)
@@ -160,7 +160,7 @@ def test_synthesiser_preserves_1D_statistics():
 
 
 def test_synthesiser_preserves_num_num_relation():
-    n_samples_orig = 1000
+    n_samples_orig = 3000
     original_data, _, _ = simulate_realistic_dataset_correlations(
         n_samples=n_samples_orig)
     synthesiser = Synthesiser(random_seed=74124)
@@ -175,7 +175,7 @@ def test_synthesiser_preserves_num_num_relation():
 
 
 def test_synthesiser_preserves_cat_num_relation():
-    n_samples_orig = 1000
+    n_samples_orig = 3000
     original_data, _, _ = simulate_realistic_dataset_correlations(
         n_samples=n_samples_orig)
     synthesiser = Synthesiser(random_seed=74124)
