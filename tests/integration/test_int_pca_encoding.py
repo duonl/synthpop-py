684d0468b8593b83e98ccfe02ece2b6ab2b25308
casvanerpduo <cas.vanerp@duo.nl>
2026-09-24 15:37:00 +0200

    Changed itnegration test to np.nan and check dtype.

--- tests/integration/test_int_pca_encoding.py
+++ tests/integration/test_int_pca_encoding.py
@@ -102,12 +102,12 @@ def test_pca_encoding_not_broken_by_setoutput_api():
     )
 
 def test_pca_encoding_fit_transform_input_and_outputs_full_nan():
-    X = np.array([np.nan, np.nan, np.nan, np.nan, np.nan], dtype= np.float32)
+    X = np.array([np.nan, np.nan, np.nan, np.nan, np.nan],dtype=str_dtype)
     y = np.array(["x", "x", "y", "z", "w"], dtype=str_dtype)
 
     encoder = PCAEncoder()
 
     result = encoder.fit_transform(X=X, y=y)
 
-    assert np.isnan(result).all() #np.isnan solely works on np.nan, not pd.NA
-    assert result.dtype == X.dtype
+    assert np.isnan(result).all() # np.isnan solely returns true on np.nan, not pd.NA
+    assert result.dtype == np.float32
